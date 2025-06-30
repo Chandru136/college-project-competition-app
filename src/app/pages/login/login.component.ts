d610123719e920d403552fb8515ddf6a9d006cbc
Chandru R <140425463+Chandru136@users.noreply.github.com>
2025-06-30 16:42:57 +0530

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  userService = inject(UserService);
+
+  onLogin() {
+    const formValue = this.loginForm.value;
+
+    this.userService.onUserLogin(formValue).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('studentId', res.userId);
+        alert('User Found');
+      },
+      error: () => {
+        alert('Wrong Credentials');
+      },
+    });
+  }
 }
