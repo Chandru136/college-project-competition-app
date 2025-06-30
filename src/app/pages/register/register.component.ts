e49bb8c3d3bca40950e59a7eddafc445ba892eff
Chandru R <140425463+Chandru136@users.noreply.github.com>
2025-06-30 16:41:50 +0530

    Update register.component.ts

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerObj: any = {
+    fullName: '',
+    email: '',
+    password: '',
+    collegeName: '',
+    role: '',
+  };
 
+  http = inject(HttpClient);
+
+  onRegister() {
+    debugger;
+    this.http
+      .post(
+        'https://api.freeprojectapi.com/api/ProjectCompetition/register',
+        this.registerObj
+      )
+      .subscribe((res: any) => {
+        debugger;
+        alert('Student Registrtion Success');
+      });
+  }
 }
